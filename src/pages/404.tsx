527288252c5fb326d19cbdcb5b8eecfc9450c3ba
Norika Okawa <n.okawa@public-id.com>
2024-03-19 10:30:40 -0700

    forgot commit

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,11 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react'
+import { graphql, PageProps } from 'gatsby'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = ({ data, location }: PageProps<GatsbyTypes.Query>) => {
+  const siteTitle = data.site?.siteMetadata?.title
 
   return (
     <Layout location={location} title={siteTitle}>
