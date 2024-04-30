6f43216ba5feb67b9f495c4bdaf1f91707f7103c
Norika Okawa <n.okawa@public-id.com>
2024-04-30 12:36:14 -0700

    add privacy page and footer link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Footer: React.FC = () => {
   return (
     <>
       <footer className="">
+        <p className="my-5 text-center text-sm">
+          <Link to="privacy">Privacy Policy</Link>
+        </p>
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
